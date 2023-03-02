c634cb095614306d9e796dc2505504ae8d2d3e7b
Eddy Matheson <rmatheson95@icloud.com>
2023-03-02 12:52:12 -0700

    added button, updated css for button, updated js for button

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const h2 = document.createElement("h2");
 h2.textContent = "G2C2P";
 
 document.querySelector("body").appendChild(h2);
+
+function beachLover() {
+  document.querySelector(".box").style.color = "blue";
+}
